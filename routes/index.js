e3ffcbd75aa0fdc3cc43ff98883e00ed709b54a1
Tam Bui <itsmetambui@gmail.com>
2022-07-30 10:11:44 +0700

    init project

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+const requestify = require("requestify");
+
+/* GET home page. */
+router.post("/", async function (req, res, next) {
+  try {
+    console.log('Send with', { inbox_id: req.body.inbox_id }, req.cookies);
+    requestify.request(
+      `https://saletend-web-git-feature-inbox-bracster-software.vercel.app/api/inbox/fetchAndSaveInboxMessages`,
+      {
+        method: 'POST',
+        body: { inbox_id: req.body.inbox_id },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        cookies: req.cookies,
+      }
+    );
+    res.json({ success: true });
+  } catch (err) {
+    res.json({ success: false });
+  }
+});
+
+module.exports = router;
